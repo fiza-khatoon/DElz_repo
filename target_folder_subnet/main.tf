d24bcf29e33e09f05dd629cc91940a78f5903501
Your Name <you@example.com>
2024-06-20 13:04:51 +0530

    Add files and commit changes

--- target_folder_subnet/main.tf
+++ target_folder_subnet/main.tf
@@ -1,29 +1,24 @@
-# Resource block for creating a virtual machine
-resource "azurerm_virtual_machine" "example" {
-  name                  = "my-vm"
-  location              = "eastus"
-  resource_group_name   = "my-resource-group"
-  vm_size               = "Standard_DS2_v2"
-  network_interface_ids = [azurerm_network_interface.example.id]
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "my-vm-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "my-vm"
-    admin_username = "adminuser"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
+# Resource block for creating a subnet
+resource "azurerm_subnet" "subnet" {
+  count                                 = length(var.subnets)
+  name                                  = var.subnets[count.index].subnet_name
+  resource_group_name                   = var.resource_group_name
+  virtual_network_name                  = var.subnets[count.index].virtual_network_name
+  address_prefixes                      = var.subnets[count.index].subnet_address_prefixes
+  service_endpoints                     = var.subnets[count.index].subnet_service_endpoints
+  private_endpoint_network_policies     = var.subnets[count.index].private_endpoint_network_policies_enabled
+  private_link_service_network_policies = var.subnets[count.index].private_link_service_network_policies_enabled
+  service_endpoint_policy_ids           = var.subnets[count.index].service_endpoint_policy_ids
+
+  dynamic "delegation" {
+    for_each = lookup(lookup(var.subnets[count.index], "subnet_delegation"), lookup(var.subnets[count.index], "subnet_name"), {})
+    content {
+      name = delegation.key
+      service_delegation {
+        name    = lookup(delegation.value, "service_name")
+        actions = lookup(delegation.value, "service_actions", [])
+      }
+    }
   }
 }
 
